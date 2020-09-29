825f8906525164a98ea398567a0e7c074d5fb87c
go-lauren <go.lauren@berkeley.edu>
2020-09-29 10:38:51 -0700

    Initial Commit

---
+++ Hangman/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hangman
+//
+//  Created by Lauren Go on 2020/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Lauren Go on 2020/09/29.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
