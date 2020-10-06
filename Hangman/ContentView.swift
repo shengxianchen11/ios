c97ba73bd3ecea48b8ba30bd862491b065c0673b
Arman Vaziri <arman@Armans-MacBook-Pro.local>
2020-10-06 00:58:22 -0700

    UI and some functions

--- Hangman/ContentView.swift
+++ Hangman/ContentView.swift
@@ -8,10 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttonValues: [[String]] = [
+        ["A", "B", "C", "D", "E", "F", "G"],
+        ["H", "I", "J", "K", "L", "M", "N"],
+        ["O", "P", "Q", "R", "S", "T", "U"],
+        ["V", "W", "X", "Y", "Z"]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Exit game")
+                }).padding(.leading, 15)
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Text("Restart")
+                }).padding(.trailing, 15)
+            }.padding(.bottom, UIScreen.main.bounds.height * 0.05)
+            
+            Image("hangman1")
+            Text("-----").font(.system(size: 50)).padding()
+            Text("Incorrect guesses: ").frame(minWidth: 0, maxWidth: .infinity, alignment: .leading).padding(.leading, 15)
+            
+            Spacer()
+            
+            ForEach(buttonValues, id: \.self) {row in
+                HStack {
+                    ForEach(row, id: \.self) {button in
+                        KeyboardButtonView(buttonLabel: button, buttonRow: row)
+                    }
+                }
+            }
+            
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top).padding(.bottom, UIScreen.main.bounds.height * 0.05)
     }
+}
+
+struct KeyboardButtonView: View {
+    
+    var buttonLabel: String
+    var buttonRow: [String]
+    
+    var body: some View {
+        
+        Button(action: {
+            // TODO: Add button tap functionality
+            
+        }, label: {
+            Text(buttonLabel)
+                .frame(width: self.buttonWidth(row: buttonRow), height: self.buttonHeight())
+        })
+        
+    }
+    
+    func buttonWidth(row: [String]) -> CGFloat {
+        if row[0] == "V" {
+            return (UIScreen.main.bounds.width - 6 * 10) / 6
+        } else {
+            return (UIScreen.main.bounds.width - 8 * 10) / 8
+        }
+    }
+    
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - 6 * 10) / 8
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Hangman/HangmanState.swift
+++ Hangman/HangmanState.swift
@@ -8,34 +8,82 @@
 import Foundation
 
 class HangmanState : ObservableObject {
+    
     @Published var index: Int = 0
     @Published var progress: String = ""
     @Published var incorrect: [Character] = []
     
+    let phrases: [String] = ["hello", "goodbye", "bears", "corona", "covid", "facemask", "macbook", "oski"]
     let validChars: String = "abcdefghijklmnopqrstuvwxyz"
+    var phrase: String!
+    var lastGuess: String = ""
     
-    var phrase: String
-    
-    init(phrase: String) {
-        self.phrase = phrase
-        self.progress = phrase.characters.map({})
+    /** Initializes a new game. */
+    init() {
+        self.phrase = self.phrases.randomElement()
+        self.progress = ""
+        self.incorrect = []
     }
     
+    /** Get functions to control access for each Hangman instance's properties. */
+    public func getPhrase() -> String { return phrase }
+    public func getGuesses() -> Int { return 6-incorrect.count }
+    public func getLastGuess() -> String { return lastGuess }
+    public func getIncorrect() -> [Character] { return incorrect }
+    public func getProgress() -> String { return progress}
+    
+    func restart() {
+        self.phrase = self.phrases.randomElement()
+        self.progress = ""
+        self.incorrect = []
+    }
+    
+    /**
+     Checks for the loser
+     - Returns: A Boolean for if the user won or not and has guesses left.
+     */
     public func didLose() -> Bool {
         return incorrect.count >= 6
     }
+    
+    /**
+     Checks for the winner.
+     - Returns: A Boolean for if the user won or not and has guesses left.
+     */
     public func didWin() -> Bool {
         return progress == phrase && incorrect.count < 6
     }
     
-    // make guess, return true if not guessed before, false if have guessed
-    func makeGuess(_ c: Character) {
-        let letter = c.lowercased()
+    /**
+     Processes the user's guess.
+     - Parameter guess letter: Character for the letter that is being guessed.
+     */
+    func makeGuess(guess letter: Character) {
         
-        if validChars.contains(c) && !progress.contains(c) {
-            
+        let lc_letter = Character(letter.lowercased())
+        lastGuess = String(lc_letter).lowercased()
+        
+        if !incorrect.contains(lc_letter) {
+            if phrase.contains(letter) {
+                let progressTemp = phrase.map{char in (char == lc_letter) || progress.contains(char) ? char :  Character("_")}
+                progress = String(progressTemp)
+            } else {
+                incorrect.append(lc_letter)
+            }
         } else {
-            
+            // TODO: show alert to user that they have already guessed that letter
         }
     }
+    
+    /**
+     Returns a message to notify the winner if they won or not
+     
+     - Returns: Message depending on whether they won or not
+     */
+    public func getFinalMessage() -> String {
+        return didWin() ? "Congrats you win!" : "Rip you lost try again"
+    }
+    
+    
+    
 }
